dca29a5cefeda1a8bce892743a5d1d35320063a1
Jakub Jira <jirajak2@fel.cvut.cz>
2023-09-13 17:24:08 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use tokio_socketcan_isotp::{IsoTpSocket, StandardId, Error};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Error> {
+    let mut rx = IsoTpSocket::open(
+        "vcan0",
+        StandardId::new(0x123).expect("Invalid src id"),
+        StandardId::new(0x321).expect("Invalid src id")
+            )?;
+
+            
+    let _ = rx.write_packet(vec![1,2,3])?.await;
+
+
+    println!("done");
+    while let Ok(mut packet) = rx.read_packet()?.await {
+        println!("{:?}", packet);
+        packet.push(0xff);
+        println!("{:?}", packet);
+        let blob = rx.write_packet(packet)?.await;
+    }
+    Ok(())
+}
